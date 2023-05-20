bc26aae351edb85972e59a97e7250c8b1b7caf57
inamnk28 <inamnkabi1@gmail>
2023-05-20 10:58:18 +0200

    secondcommit

--- script/index.js
+++ script/index.js
@@ -1,13 +1,19 @@
 let yourview = document.getElementById("space")
 
-const view = (number) => {
-    yourview.value += number ;
-}
+//const view = (number) => {
+    //yourview.value += number ;
+//}
 let answer = () => {
-    yourview.value = eval(yourview.value)
+  yourview.value = eval(yourview.value)
 }
 function nothing() {
     yourview.value = '';
 }
 
+function view(num) {
+    yourview.value += num;
+}
 
+function del() {
+    yourview.value = yourview.value.slice(0,-1);
+}
